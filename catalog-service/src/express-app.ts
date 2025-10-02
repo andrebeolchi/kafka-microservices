3007005ba61a1f2f5748b3bf23a2c6fa7b705457
Andre Beolchi <andregarbini2001@gmail.com>
2025-10-01 21:53:47 -0300

    feat(catalog-service): add CORS support to express app and update dependencies

--- catalog-service/src/express-app.ts
+++ catalog-service/src/express-app.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import { router as catalogRoutes } from './api/catalog-routes'
 import { HandleErrorWithLogger } from './utils/error/handler'
 import { httpLogger } from './utils/logger'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors({ origin: '*' }))
 app.use(httpLogger)
 
 app.use(catalogRoutes)
--- order-service/src/express-app.ts
+++ order-service/src/express-app.ts
@@ -9,7 +9,7 @@ import { initializeBroker } from './services/broker'
 
 export const expressApp = async () => {
   const app = express()
-  app.use(cors())
+  app.use(cors({ origin: '*' }))
   app.use(express.json())
   app.use(httpLogger)
 
